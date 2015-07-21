2762f0c9a040345c260db46a060811af99371d06
Emily Blanchard <emblou2@aol.com>
2015-07-21 13:44:17 -0600

    Added slugs for URLs

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,6 +3,12 @@ class PinsController < ApplicationController
   def index
     @pins = Pin.all
   end
+
+   def show_by_name
+    @pin = Pin.find_by_slug(params[:slug])
+    render :show
+  end
+
   
   def show
     @pin = Pin.find(params[:id])
