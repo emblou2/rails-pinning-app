e8462a0e43fd97cff353006987e13d97c7dfe399
Emily Blanchard <emblou2@aol.com>
2015-07-27 08:59:43 -0600

    Added new form to create pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,5 +13,26 @@ class PinsController < ApplicationController
   def show
     @pin = Pin.find(params[:id])
   end
-  
+
+   def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to "/pins/name-#{@pin.slug}"
+    else
+      @errors = @pin.errors
+      render :new
+    end
+  end
+
+
+  private
+  def pin_params
+    params.require(:pin).permit(:title, :url, :text, :slug, :category_id)
+  end
 end
+  
